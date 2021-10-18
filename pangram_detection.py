f57d5749dedaecf472272b4a741f97c18f0d3e22
Dominick Campanile <Dominick@Dominicks-MacBook-Air.local>
2021-10-18 10:01:42 -0500

    new way to do it

--- pangram_detection.py
+++ pangram_detection.py
@@ -12,18 +12,29 @@
 # Output: false
 str1= 'abcdefghijklmnopqurstuvxwyz'
 def check_pangram(str):
-  if len(set('abcdefghijklmnopqrstuvwxyz') - set(str.lower())) ==  0 :
-    return True
+    if len(set('abcdefghijklmnopqrstuvwxyz') - set(str.lower())) ==  0 :
+        return True
 
-  return False
+    return False
 
 if(check_pangram('the quick brown fox jumps over the lazy dog')):
-  print("This is a pangram")
+    print("This is a pangram")
 else:
-  print("This is not a pangram")
+    print("This is not a pangram")
 if(check_pangram('leetcode')):
-  print("This is a pangram")
+    print("This is a pangram")
 else:
-  print("This is not a pangram")
+    print("This is not a pangram")
 
 
+def pana(st):
+    my_list = []
+    for i in st:
+        if i not in my_list:
+            my_list.append(i)
+    if len(my_list) == 26:
+        return True
+    return False
+print(pana("thequickbrownfoxjumpsoverthelazydog"))
+print(pana("leetcode"))
+print(pana("qweryuiopasdfghjklzxcvbnnncdsfsdf"))
